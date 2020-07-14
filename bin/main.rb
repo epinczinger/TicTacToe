88cc3c3bbff100b5bc6e24ca6b6ebfc55654273c
EstebanP <epinczinger@gmail.com>
2020-07-14 17:53:15 -0300

    fix rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
-puts 'Hello World'
+
 require 'colorize'
 
 class User
   attr_accessor :name
-  attr_reader :s
-  def initialize (s)
-    print " Write you name: "
-    @name= gets.chomp
-    @s=s
-    puts "Welcome #{@name}. Your symbol is #{@s}"
+  attr_reader :symb
+  def initialize(symb)
+    print ' Write your name: '
+    @name = gets.chomp
+    @symb = symb
+    puts "Welcome #{@name}. Your symbol is #{@symb}"
   end
 end
 
@@ -18,35 +18,35 @@ class Board
 
   def initialize
     # @b=Array.new(3) {|i| i=Array.new(3){|j| j=" "}}
-    @b=[%w[1 2 3],%w[4 5 6],%w[7 8 9]]
-    @counter=9
+    @b = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
+    @counter = 9
   end
 
   def display
-    3.times {|i| p @b[i]}
+    3.times { |i| p @b[i] }
   end
 
-  def turn(s,loc)
-    @b[(loc-1)/3][(loc-1)%3]=s
+  def turn(_symb, loc)
+    @b[(loc - 1) / 3][(loc - 1) % 3] = symb
   end
 end
 
+puts '*********************************'
+puts '****      TIC TAC TOE        ****'
+puts '*********************************'
+user = []
+user << User.new('X')
+user << User.new('O')
 
-puts "*********************************"
-puts "***       TIC TAC TOE        ****"
-puts "*********************************"
-user=[]
-user << User.new("X")
-user << User.new("O")
-
-board=Board.new
+board = Board.new
 board.display
-while board.counter>0
-cu=board.counter%2
-print "#{user[cu].name} select the number of an empty box:"
-n=gets.chomp.to_i
-board.turn(user[cu].s,n)
-puts "***********************************"
-board.display 
-board.counter-=1
+
+while board.counter.positive?
+  cu = board.counter % 2
+  print "#{user[cu].name} select the number of an empty box:"
+  n = gets.chomp.to_i
+  board.turn(user[cu].symb, n)
+  puts '***********************************'
+  board.display
+  board.counter -= 1
 end
